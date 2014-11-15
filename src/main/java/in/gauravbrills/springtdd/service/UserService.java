c0bd4d5069cac7f3f0ada4111a661acb7584a26e
grawat <gauravbrills14@gmail.com>
2014-11-15 23:34:38 +0530

    User service documented

--- src/main/java/in/gauravbrills/springtdd/service/UserService.java
+++ src/main/java/in/gauravbrills/springtdd/service/UserService.java
@@ -1,15 +1,41 @@
+/*
+ * UserService.java
+ * 
+ * Copyright Gaurav Rawat 2014
+ *
+ * @author Gaurav Rawat
+ */
 package in.gauravbrills.springtdd.service;
 
 import in.gauravbrills.springtdd.model.User;
 
 import java.util.List;
 
+/**
+ * The Interface UserService.
+ */
 public interface UserService {
 
+	/**
+	 * Gets the all users.
+	 *
+	 * @return the all users
+	 */
 	List<User> getAllUsers();
 
+	/**
+	 * Save.
+	 *
+	 * @param user the user
+	 */
 	void save(User user);
 
+	/**
+	 * Gets the by name.
+	 *
+	 * @param name the name
+	 * @return the by name
+	 */
 	User getByName(String name);
 
 }
